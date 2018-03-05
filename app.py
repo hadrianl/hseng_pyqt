305cc61803353a5ae866b56912c32a80657ca1f0
hadrianl <137150224@qq.com>
2018-03-05 17:13:12 +0800

    添加注译，修改了数据获取代码，按时间来生成当前期货代码

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from data_fetch.market_data import OHLC, NewOHLC
 from data_handle.indicator import Ma, Macd, Std
 from data_visualize.OHLC_ui import OHlCWidget
 from data_visualize.console import AnalysisConsole
+from data_visualize.Login_ui import LoginDialog
+from data_fetch.util import MONTH_LETTER_MAPS
 import datetime as dt
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
@@ -17,48 +19,57 @@ import sys
 
 pg.setConfigOptions(leftButtonPan=True, crashWarning=True)
 # ------------------------------数据获取与整理---------------------------+
+# 确定需要展示的K先范围
 start_time = dt.datetime.now() - dt.timedelta(minutes=120)
 end_time = dt.datetime.now() + dt.timedelta(minutes=10)
-ohlc = OHLC(start_time, end_time, 'HSIG8')
+symbol = 'HSI' + MONTH_LETTER_MAPS[dt.datetime.now().month] + str(dt.datetime.now().year)[-1]  # 根据当前时间生成品种代码
+# 初始化主图的历史ohlc，最新ohlc与指标数据的参数配置
+ohlc = OHLC(start_time, end_time, 'HSIH8')
+tick_datas = NewOHLC('HSIH8')
 i_macd = Macd(short=10, long=22, m=9)
 i_ma = Ma(ma10=10, ma20=20, ma30=30, ma60=60)
 i_std = Std(window=60, min_periods=2)
+# 将指标假如到主图数据里
 ohlc + i_ma
 ohlc + i_macd
 ohlc + i_std
-tick_datas = NewOHLC('HSIG8')
-
 # -----------------------------------------------------------------------+
 # -----------------------窗口与app初始化---------------------------------+
 app = QtWidgets.QApplication(sys.argv)
 win = OHlCWidget()
+win.setWindowTitle(symbol + '实盘分钟图')
 # 各个初始化之间存在依赖关系，需要按照以下顺序初始化
-win.binddata(ohlc=ohlc, tick_datas=tick_datas, i_ma=i_ma, i_macd=i_macd, i_std=i_std)
+win.binddata(ohlc=ohlc, tick_datas=tick_datas, i_ma=i_ma, i_macd=i_macd, i_std=i_std)  # 把数据与UI绑定
 win.init_ohlc()  # 初始化ohlc主图
 win.init_ma()  # 初始化ma均线图
 win.init_indicator()  # 初始化指标
 win.init_std()  # 初始化std指标
 win.init_date_slice()  # 初始化时间切片
-win.init_mouseaction()
+win.init_mouseaction()  # 初始化十字光标与鼠标交互
 win.init_signal()  # 初始化指标
 
-win.tick_datas.active()
+win.tick_datas.active()  # 启动tick_datas
 win.date_region.setRegion([win.ohlc.timeindex.max() - 120, win.ohlc.timeindex.max() + 5])  # 初始化可视区域
 win.tick_datas._timeindex = ohlc.timeindex.iloc[-1] + 1
-win.ohlc_data_update_sync()
+win.ohlc_data_update_sync()  # 主图的横坐标的初始化刷新调整
 
-namespace = {'ohlc': ohlc, 'i_macd': i_macd, 'tick_datas': tick_datas, 'win': win}
+namespace = {'ohlc': ohlc, 'i_macd': i_macd, 'tick_datas': tick_datas, 'win': win}  # console的命名空间
 console = AnalysisConsole(namespace)
 
 
 if __name__ == '__main__':
+    from login import Ui_LoginWindow
+    from PyQt5.Qt import QDialog
     def show():
         if console.isHidden():
             console.show()
         else:
             console.focusWidget()
     win.resize(1200, 800)
-    win.show()
-    win.sig_M_Left_Double_Click.connect(show)
+    login_win = LoginDialog()
+    login_win.show()
+    login_win.accepted.connect(win.show)
+    login_win.rejected.connect(app.closeAllWindows)
+    win.sig_M_Left_Double_Click.connect(show)  # 绑定双击行为调出console
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         sys.exit(app.exec())
