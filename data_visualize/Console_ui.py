356e2334aaa9cba0b3e488155e122455c375c2d2
hadrianl <137150224@qq.com>
2018-03-20 13:09:36 +0800

    为console交互界面加入了重取样选项，已经日期范围选项

---
+++ data_visualize/Console_ui.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/31 0031 15:12
+# @Author  : Hadrianl 
+# @File    : Console_ui.py
+# @License : (C) Copyright 2013-2017, 凯瑞投资
+
+
+import pyqtgraph.console
+from util import V_logger
+from console import *
+from PyQt5.Qt import QWidget
+
+# class AnalysisConsole(pyqtgraph.console.ConsoleWidget):
+#     def __init__(self, namespace):
+#         text = f'''实盘分析Console测试（help_doc()调用帮助文档）'''
+#         super(AnalysisConsole,self).__init__(namespace=namespace, text=text)
+#         self.setWindowTitle(text)
+#         V_logger.info(f'初始化console交互界面')
+#
+#     def focus(self):
+#         if self.isHidden():
+#             self.show()
+#         else:
+#             self.focusWidget()
+
+
+class AnalysisConsole(QWidget, Ui_Console):
+    def __init__(self, namespace):
+        QWidget.__init__(self)
+        Ui_Console.__init__(self)
+        help_text = f'''实盘分析Console测试（help_doc()调用帮助文档）'''
+        self.setupUi(self)
+        self.consolewidget.localNamespace = namespace
+        self.consolewidget.output.setPlainText(help_text)
+
+    def focus(self):
+        if self.isHidden():
+            self.show()
+        else:
+            self.focusWidget()
