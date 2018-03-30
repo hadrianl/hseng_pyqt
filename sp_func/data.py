b6696d54bae63423c149c70baab9a5785f616be0
hadrianl <137150224@qq.com>
2018-03-30 14:29:57 +0800

    加入了sp_func的data类，用于向服务器请求账户的资讯数据

---
+++ sp_func/data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/30 0030 14:14
+# @Author  : Hadrianl 
+# @File    : data.py
+# @License : (C) Copyright 2013-2017, 凯瑞投资
+
+
+from spapi.sub_client import SubPrice, SubTicker, _s
+
+# def get_order
